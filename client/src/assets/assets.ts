d63a90b54de53a3f813200f624f785c1259c4f4f
cang688 <cang688@aucklanduni.ac.nz>
2026-04-14 07:47:04 +1200

    created basic dashboard page and set up routes, may need update in future

--- client/src/assets/assets.ts
+++ client/src/assets/assets.ts
@@ -3,6 +3,9 @@ import profile from './profile.png'
 import dropdown from './dropdown.png'
 import profileIcon from './profile-icon.png'
 import logoutIcon from './logout-icon.png'
+import surveyIcon from './survey_icon.png'
+import completedIcon from './completed_icon.png'
+import dataIcon from './data_icon.png'
 
 export const assets = {
     logo,
@@ -10,4 +13,7 @@ export const assets = {
     dropdown,
     profileIcon,
     logoutIcon,
+    surveyIcon,
+    completedIcon,
+    dataIcon
 }
